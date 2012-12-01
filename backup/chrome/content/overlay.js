6885d66be85c8976eeacb59123819a36467b408c
Oyayubi11 <qqaq72269@gmail.com>
2012-12-02 01:28:09 +0900

    add filtering by mail address

--- backup/chrome/content/overlay.js
+++ backup/chrome/content/overlay.js
@@ -1,6 +1,148 @@
+var configFileName = "backupMail.conf"; //コンフィグファイル名は決め打ち
+var configFlagAcc  = false;  //アカウント指定がある場合true
+var configFlagAddr = false;  //アドレス指定がある場合true
+
+var hashAccount = new Array();
+var hashAddress = new Array();
+
+var myPanel;
+var path;
+
+//<..@..>から..@..だけ抜き出し
+function chompUrl( url ){
+    let s = url.indexOf("<");
+    if( s < 0 ){
+	return url;
+    }  
+    let e = url.indexOf(">");
+    if( e < 0 ){
+	return url;
+    }
+    let v = url.substring(s+1,e).trim();
+    return v;
+}
+
+function msgHdrGetUri (aMsg) {
+    return aMsg.folder.getUriForMsg(aMsg)
+}
+
+function getMsgHdr(aMsgHdr, k){
+    let uri = msgHdrGetUri(aMsgHdr);
+    let messenger = Components.classes["@mozilla.org/messenger;1"]
+        .createInstance(Components.interfaces.nsIMessenger);
+    let messageService = messenger.messageServiceFromURI(uri);
+    
+    let fallback = function ()
+    MsgHdrToMimeMessage(aMsgHdr, null, function (aMsgHdr, aMimeMsg) {
+	k(aMimeMsg);
+    }, true, {
+	partsOnDemand: true,
+    });
+
+  // This is intentionally disabled because there's a bug in Thunderbird that
+  // renders the supposedly-useful streamHeaders function unusable.
+    if (false && "streamHeaders" in messageService) {
+	try {
+	    messageService.streamHeaders(uri, createStreamListener(function (aRawString) {
+		let re = /\r?\n\s+/g;
+		let str = aRawString.replace(re, " ");
+		let lines = str.split(/\r?\n/);
+		let obj = {};
+		for each (let [, line] in Iterator(lines)) {
+		    let i = line.indexOf(":");
+		    if (i < 0)
+			continue;
+		    let k = line.substring(0, i).toLowerCase();
+		    let v = line.substring(i+1).trim();
+		    if (!(k in obj))
+			obj[k] = [];
+		    obj[k].push(v);
+		}
+		k(new HeaderHandler(obj));
+	    }), null, true);
+	} catch (e) {
+	    fallback();
+	}
+    } else {
+	fallback();
+    }
+}
+
+function getHeader(MsgBody, Header){
+   
+}
+
+function getFrom(MsgBody){
+
+}
+
+function getDeliverdTo(MsgBody){
+
+}
+
+function getCC(MsgBody){
+
+}
+
+function getBCC(MsbBody){
+
+}
+
+function getConfigFile(){
+    configFlagAcc = false;
+    configFlagAddr= false;
+
+    Components.utils.import("resource://gre/modules/FileUtils.jsm");
+    Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+    //Preferencesから保存先ディレクトリ読み出し
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService).getBranch("extensions.backup.");
+    var value = prefs.getCharPref("stringpref");
+
+    var file    = new FileUtils.File(value + configFileName);
+
+    //コンフィグファイルがなければ何もしない
+    if( !file.exists() ){
+	Application.console.log("can't find config file");
+	return;
+    }
+    
+    NetUtil.asyncFetch(file, function(inputStream, status) {
+	if (!Components.isSuccessCode(status)) {
+	    // Handle error!
+	    Application.console.log("error read file");
+	    return;
+	}
+ 
+	// The file data is contained within inputStream.
+	// You can read it into a string with
+	var data = NetUtil.readInputStreamToString(inputStream, inputStream.available());
+	
+	//コンフィグファイルから設定読み出し：メールアカウント設定
+	var array = data.split(/\r\n|\r|\n/);
+	if( array.length > 0 ){
+	    for( var i = 0; i < array.length; i++ ){
+		if( array[i].match(/(account:)(.+)/) ) {
+		    hashAccount[RegExp.$2.trim()] = true;
+		    configFlagAcc = true;
+		}else if( array[i].match(/(address:)(.+)/)) {
+		    hashAddress[RegExp.$2.trim()] = true;	
+		    configFlagAddr= true;
+		}
+	    }
+	}else{
+	    Application.console.log("config error");
+	}
+	return;
+    });
+    return;
+}
 
 //保存先ディレクトリのチェック、作成、パス名リターン
-function checkAndgetDir(myPanel){
+//フォルダが移動、日付が変わる可能性があるため、
+//都度チェックするようにする。
+function checkAndgetDir(){
     Components.utils.import("resource://gre/modules/FileUtils.jsm");
     Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
@@ -9,7 +151,6 @@ function checkAndgetDir(myPanel){
         .getService(Components.interfaces.nsIPrefService).getBranch("extensions.backup.");
     var value = prefs.getCharPref("stringpref");
     
-    Application.console.log(value);
     var dir    = new FileUtils.File(value);
     
     //コンフィグ指定の保存先dir存在チェック
@@ -52,24 +193,8 @@ function getFileName( aMsgHdr ){
     return aMsgHdr.messageId + ".eml";
 }
 
-//Mail受信時の動作を追加
-var atMailReceived = {
-    msgAdded: function(aMsgHdr) {
-        if( !aMsgHdr.isRead ){
-	    ///右下ステータスバーの自己定義要素取得
-	    var myPanel = document.getElementById("my-panel");
-	    myPanel.label = "Start save";
-
-	    Components.utils.import("resource://gre/modules/FileUtils.jsm");
-	    Components.utils.import("resource://gre/modules/NetUtil.jsm");
-
-	    var path = checkAndgetDir(myPanel);
-
-	    if( path == "" ){
-		return;
-	    }
-
-	    //日付ディレクトリ準備OKなので、データ書き込み
+//メール保存処理
+function saveMail(aMsgHdr){
 	    let messenger = Components.classes["@mozilla.org/messenger;1"].createInstance(Components.interfaces.nsIMessenger);
 	    var mailSaveListener = { messageID: "",
 				     OnStartRunningUrl: function(url){},
@@ -93,6 +218,7 @@ var atMailReceived = {
 	    //messageServiceFromURI : uriからnsIMsgMessageServiceを取得
 	    //SaveMessageToDisk : メッセージをディスクへ保存
 	    var aMsgSvc = messenger.messageServiceFromURI(uri)
+	    
 	    var saveUri = aMsgSvc.SaveMessageToDisk(uri,   //保存するメッセージのuri
 						    path,  //保存先ファイル情報
 						    false, //特別な事情がない限りfalseでOK
@@ -100,14 +226,65 @@ var atMailReceived = {
 						    outuri,  //謎
 						    false, //謎
 						    null);   //progress and status feedback
+}
+
+//Mail受信時の動作を追加
+var atMailReceived = {
+    msgAdded: function(aMsgHdr) {
+        if( !aMsgHdr.isRead ){
+	    ///右下ステータスバーの自己定義要素取得
+	    myPanel = document.getElementById("my-panel");
+	    myPanel.label = "Start save";
+
+	    Components.utils.import("resource://gre/modules/FileUtils.jsm");
+	    Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+	    //check backup dir
+	    path = checkAndgetDir();
+
+	    if( path == "" ){
+		return;	    //backup dir check error
+	    }
+
+	    //日付ディレクトリ準備OKなので、データ書き込み
+	    //configがあればメールからヘッダー情報抜き取り
+	    if( (configFlagAcc||configFlagAddr) ){
+		getMsgHdr( aMsgHdr, function(aMimeMsg) {
+		    var to, from;
+		    if( configFlagAcc && aMimeMsg.has("to") ){
+			to = chompUrl(aMimeMsg.get("to"));
+		    }
+		    if( configFlagAddr && aMimeMsg.has("from") ){
+			from = chompUrl(aMimeMsg.get("from"));
+		    }
+		    // アカウントとtoヘッダ     宛先が自アカウントを想定
+		    // 指定アドレスとfromヘッダ 送信元アドレスの絞り込み
+		    // 指定アドレスとtoヘッダ   メーリス対策でアドレス絞り込み
+		    // を比較する
+		    if( hashAccount[to] || hashAddress[from] || hashAddress[to] ){
+			myPanel.label = "match";
+			saveMail(aMsgHdr);
+		    }else{
+			myPanel.label = "not match";
+		    }
+
+		} );
+	    }else{ //config無いときはそのまま保存
+		saveMail(aMsgHdr)
+	    }
 	}
     }
 }
  
 function startup() {
+    //setup Config
+    getConfigFile();
+
+    //initialize notification panel
     var myPanel = document.getElementById("my-panel");
     myPanel.label = "Mail backup tool is loaded";
 
+    //initialize callback funtion at mail received.
     var notificationService =
 	Components.classes["@mozilla.org/messenger/msgnotificationservice;1"]
 	.getService(Components.interfaces.nsIMsgFolderNotificationService);
